96ad706f632d320ed2a912968ad7aa9c01852bea
davoros <ostojic.davor@gmail.com>
2016-02-16 15:17:14 +0100

    cl params for folder or file

--- src/main/java/eu/clarin/cmdi/curation/main/Main.java
+++ src/main/java/eu/clarin/cmdi/curation/main/Main.java
@@ -57,22 +57,11 @@ public class Main {
     }
 
     public static void main(String[] args) {
-	Path path1 = FileSystems.getDefault().getPath(
-		"D:/data/cmdi/META_SHARE_3_0/08ae069e770a11e5a6e4005056b4002410c2850320274e04b9a6c6692883a054.xml");
-	Path path2 = FileSystems.getDefault().getPath("D:/data/cmdi/Deutsches_Textarchiv/dta_386.xml");
-	Path cmdi = FileSystems.getDefault().getPath("D:/data/cmdi");
-	Path test = FileSystems.getDefault().getPath("D:/data/test");
-
-	Path ehu_18 = FileSystems.getDefault().getPath("D:/data/cmdi/Euskal_Herriko_Unibertsitatea");
-	Path lbof_295 = FileSystems.getDefault().getPath("D:/data/cmdi/Language_Bank_of_Finland");
-	Path eloftw_7K = FileSystems.getDefault().getPath("D:/data/cmdi/Ethnologue_Languages_of_the_World");
-	Path bas_23K = FileSystems.getDefault().getPath("D:/data/cmdi/BAS_Repository/oai_BAS_repo_Corpora_aGender_101104.xml");
-	Path unname_65K = FileSystems.getDefault().getPath("D:/data/cmdi/Unnamed_provider_at_dspace_library_uu_nl");
-	Path mee_240K = FileSystems.getDefault().getPath("D:/data/cmdi/Meertens_Institute_Metadata_Repository");
-
+	
 	Main curator = new Main();
-	// curator.curate(path1); curator.curate(path2);
-	curator.curate(unname_65K);
+	
+	for(String entity: args)
+	    curator.curate(FileSystems.getDefault().getPath(entity));
     }
 
 }
