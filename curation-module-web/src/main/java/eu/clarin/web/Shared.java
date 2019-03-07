5714155fc019b9dd1b897029d3c46d49405f7a27
Can <canyil@gmail.com>
2019-03-07 17:08:16 +0100

    add temp and instance report folder cleaning service. Every day at 3 am, files older than 24 hours are deleted.

--- curation-module-web/src/main/java/eu/clarin/web/Shared.java
+++ curation-module-web/src/main/java/eu/clarin/web/Shared.java
@@ -26,7 +26,15 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 public class Shared {
@@ -49,6 +57,8 @@ public class Shared {
         initCollections();
         initCollectionStatistics();
         initLinkCheckerStatistics();
+
+        initTempCleaner();
     }
 
     public static CollectionReport getCollectionReport(final String name) {
@@ -176,6 +186,55 @@ public class Shared {
         } catch (IOException e) {
             _logger.error("Problem writing to the statistics.html");
         }
+    }
+
+    static class TempCleaner extends Thread {
+
+        @Override
+        public void run() {
+
+            try (DirectoryStream<Path> ds = Files.newDirectoryStream(Paths.get(Configuration.OUTPUT_DIRECTORY + "/instances/"))) {
+                for (Path path : ds) {
+                    File file = new File(path.toString());
+                    LocalDateTime lastModified = LocalDateTime.ofInstant(Instant.ofEpochMilli(file.lastModified()), ZoneId.systemDefault());
+                    LocalDateTime now = LocalDateTime.ofInstant(Instant.ofEpochMilli(System.currentTimeMillis()), ZoneId.systemDefault());
+                    if (lastModified.plusDays(1).isBefore(now)) {
+                        Files.deleteIfExists(path);
+                    }
+                }
+            } catch (IOException e) {
+                _logger.error("Error cleaning instances folder: " + e.getMessage());
+            }
+
+
+            try (DirectoryStream<Path> ds = Files.newDirectoryStream(Paths.get(System.getProperty("java.io.tmpdir")))) {
+                for (Path path : ds) {
+                    if (path.toString().endsWith(".xml")) {
+                        File file = new File(path.toString());
+                        LocalDateTime lastModified = LocalDateTime.ofInstant(Instant.ofEpochMilli(file.lastModified()), ZoneId.systemDefault());
+                        LocalDateTime now = LocalDateTime.ofInstant(Instant.ofEpochMilli(System.currentTimeMillis()), ZoneId.systemDefault());
+                        if (lastModified.plusDays(1).isBefore(now)) {
+                            Files.deleteIfExists(path);
+                        }
+                    }
+                }
+            } catch (IOException e) {
+                _logger.error("Error cleaning temp folder: " + e.getMessage());
+            }
+
+        }
+
+    }
+
+    //This method cleans the temp folder and the instance report folder once a day at 03:00.
+    private static void initTempCleaner() {
+
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+        Long threeAM = LocalDateTime.now().until(LocalDate.now().plusDays(1).atTime(3, 0), ChronoUnit.MINUTES);
+
+        TempCleaner cleaner = new TempCleaner();
+        scheduler.scheduleAtFixedRate(cleaner, threeAM, TimeUnit.DAYS.toMinutes(1), TimeUnit.MINUTES);
     }
 
 }
