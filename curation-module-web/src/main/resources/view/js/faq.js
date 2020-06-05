5bcad1140768337dd1476e171c01d77d69c44c85
coy123 <canyil@gmail.com>
2020-06-05 13:27:07 +0200

    faq is taken from github first, then server if github doesnt work

--- curation-module-web/src/main/resources/view/js/faq.js
+++ curation-module-web/src/main/resources/view/js/faq.js
@@ -1,9 +1,20 @@
 $(document).ready( function () {
-    $.ajax({url: "/faq/faq.md", success: function(result){
-        //convert md to html with showdownjs and return
-        var converter = new showdown.Converter();
-        var html = converter.makeHtml(result);
 
-        $("#faq").html(html);
-      }});
+    $.ajax({url: "https://raw.githubusercontent.com/clarin-eric/clarin-curation-module/master/curation-module-web/src/main/resources/view/markdown/faq.md",
+        success: function(result){
+            $("#faq").html(convert(result));
+        },
+        error: function(XMLHttpRequest, textStatus, errorThrown){
+            //if github link doesnt work, get it from resources (this way, github can be updated without redeploying)
+            $.ajax({url: "/faq/faq.md", success: function(result){
+                $("#faq").html(convert(result));
+            }});
+        }
+    });
 });
+
+//convert md to html with showdownjs and return
+function convert(mdFile){
+        var converter = new showdown.Converter();
+        return converter.makeHtml(mdFile);
+}
