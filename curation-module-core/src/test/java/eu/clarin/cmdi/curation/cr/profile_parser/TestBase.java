2a7f3c24daefdca39eac02f04e91397e04c81601
WolfgangWalter Sauer (wowasa) <wolfgang.sauer@oeaw.ac.at>
2021-09-04 22:12:42 +0200

    enlarging byteSize to BIGINT/long

--- curation-module-core/src/test/java/eu/clarin/cmdi/curation/cr/profile_parser/TestBase.java
+++ curation-module-core/src/test/java/eu/clarin/cmdi/curation/cr/profile_parser/TestBase.java
@@ -59,28 +59,28 @@ public class TestBase {
             Configuration.linkToBeCheckedResource.save(new LinkToBeChecked("https://maps.google.com", "GoogleRecord", "Google", null, today));
             Configuration.linkToBeCheckedResource.save(new LinkToBeChecked("https://drive.google.com", "GoogleRecord", "Google", null, today));
             
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.ailla.org/waiting.html", "HEAD", 200, "text/html; charset=UTF-8", 100, 132, today, "Ok", 0, Category.Ok));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.ailla.org/audio_files/EMP1M1B1.mp3", "GET",  400, "text/html; charset=UTF-8", 0, 46, today, "Broken", 0, Category.Broken));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.ailla.org/audio_files/WBA1M3A2.mp3", "GET",  400, "text/html; charset=UTF-8", 0, 46, today, "Broken", 0, Category.Broken));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.ailla.org/text_files/WBA1M1A2a.mp3", "GET",  400, "text/html; charset=UTF-8", 0, 46, today, "Broken", 0, Category.Broken));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.ailla.org/audio_files/KUA2M1A1.mp3", "GET",  400, "text/html; charset=UTF-8", 0, 56, today, "Broken", 0, Category.Broken));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.ailla.org/text_files/KUA2M1.pdf", "HEAD",  200, "text/html; charset=UTF-8", 0, 51, today, "Ok", 0, Category.Ok));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.ailla.org/audio_files/sarixojani.mp3", "GET",  400, "text/html; charset=UTF-8", 0, 48, today, "Broken", 0, Category.Broken));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.ailla.org/audio_files/TEH11M7A1sa.mp3", "GET",  400, "text/html; charset=UTF-8", 0, 48, today, "Broken", 0, Category.Broken));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.ailla.org/text_files/TEH11M7.pdf", "HEAD",  200, "text/html; charset=UTF-8", 0, 57, today, "Ok", 0, Category.Ok));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://dspin.dwds.de:8088/ddc-sru/dta/", "HEAD",  200, "application/xml;charset=utf-8", 2094, 67, today, "Ok", 0, Category.Ok));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://dspin.dwds.de:8088/ddc-sru/grenzboten/", "HEAD",  200, "application/xml;charset=utf-8", 2273, 57, today, "Ok", 0, Category.Ok));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://dspin.dwds.de:8088/ddc-sru/rem/", "HEAD",  200, "application/xml;charset=utf-8", 2497, 58, today, "Ok", 0, Category.Ok));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.deutschestextarchiv.de/rem/?d=M084E-N1.xml", "HEAD",  200, "text/html; charset=utf-8", 0, 591, today, "Ok", 0, Category.Ok));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.deutschestextarchiv.de/rem/?d=M220P-N1.xml", "HEAD",  200, "text/html; charset=utf-8", 0, 592, today, "Ok", 0, Category.Ok));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.deutschestextarchiv.de/rem/?d=M119-N1.xml", "HEAD",  200, "text/html; charset=utf-8", 0, 602, today, "Ok", 0, Category.Ok));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.deutschestextarchiv.de/rem/?d=M171-G1.xml", "HEAD",  200, "text/html; charset=utf-8", 0, 613, today, "Ok", 0, Category.Ok));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.deutschestextarchiv.de/rem/?d=M185-N1.xml", "HEAD",  200, "text/html; charset=utf-8", 0, 605, today, "Ok", 0, Category.Ok));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.deutschestextarchiv.de/rem/?d=M048P-N1.xml", "HEAD",  200, "text/html; charset=utf-8", 0, 599, today, "Ok", 0, Category.Ok));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.deutschestextarchiv.de/rem/?d=M112-G1.xml", "HEAD",  200, "text/html; charset=utf-8", 0, 591, today, "Ok", 0, Category.Ok));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "https://www.google.com", "HEAD",  200, "text/html; charset=ISO-8859-1", 0, 222, today, "Ok", 0, Category.Ok));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "https://maps.google.com", "HEAD",  200, "text/html; charset=UTF-8", 0, 440, today, "Ok", 2, Category.Ok));
-            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "https://drive.google.com", "HEAD",  200, "text/html; charset=UTF-8", 73232, 413, today, "Ok", 1, Category.Ok));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.ailla.org/waiting.html", "HEAD", 200, "text/html; charset=UTF-8", 100l, 132, today, "Ok", 0, Category.Ok));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.ailla.org/audio_files/EMP1M1B1.mp3", "GET",  400, "text/html; charset=UTF-8", 0l, 46, today, "Broken", 0, Category.Broken));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.ailla.org/audio_files/WBA1M3A2.mp3", "GET",  400, "text/html; charset=UTF-8", 0l, 46, today, "Broken", 0, Category.Broken));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.ailla.org/text_files/WBA1M1A2a.mp3", "GET",  400, "text/html; charset=UTF-8", 0l, 46, today, "Broken", 0, Category.Broken));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.ailla.org/audio_files/KUA2M1A1.mp3", "GET",  400, "text/html; charset=UTF-8", 0l, 56, today, "Broken", 0, Category.Broken));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.ailla.org/text_files/KUA2M1.pdf", "HEAD",  200, "text/html; charset=UTF-8", 0l, 51, today, "Ok", 0, Category.Ok));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.ailla.org/audio_files/sarixojani.mp3", "GET",  400, "text/html; charset=UTF-8", 0l, 48, today, "Broken", 0, Category.Broken));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.ailla.org/audio_files/TEH11M7A1sa.mp3", "GET",  400, "text/html; charset=UTF-8", 0l, 48, today, "Broken", 0, Category.Broken));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.ailla.org/text_files/TEH11M7.pdf", "HEAD",  200, "text/html; charset=UTF-8", 0l, 57, today, "Ok", 0, Category.Ok));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://dspin.dwds.de:8088/ddc-sru/dta/", "HEAD",  200, "application/xml;charset=utf-8", 2094l, 67, today, "Ok", 0, Category.Ok));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://dspin.dwds.de:8088/ddc-sru/grenzboten/", "HEAD",  200, "application/xml;charset=utf-8", 2273l, 57, today, "Ok", 0, Category.Ok));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://dspin.dwds.de:8088/ddc-sru/rem/", "HEAD",  200, "application/xml;charset=utf-8", 2497l, 58, today, "Ok", 0, Category.Ok));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.deutschestextarchiv.de/rem/?d=M084E-N1.xml", "HEAD",  200, "text/html; charset=utf-8", 0l, 591, today, "Ok", 0, Category.Ok));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.deutschestextarchiv.de/rem/?d=M220P-N1.xml", "HEAD",  200, "text/html; charset=utf-8", 0l, 592, today, "Ok", 0, Category.Ok));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.deutschestextarchiv.de/rem/?d=M119-N1.xml", "HEAD",  200, "text/html; charset=utf-8", 0l, 602, today, "Ok", 0, Category.Ok));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.deutschestextarchiv.de/rem/?d=M171-G1.xml", "HEAD",  200, "text/html; charset=utf-8", 0l, 613, today, "Ok", 0, Category.Ok));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.deutschestextarchiv.de/rem/?d=M185-N1.xml", "HEAD",  200, "text/html; charset=utf-8", 0l, 605, today, "Ok", 0, Category.Ok));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.deutschestextarchiv.de/rem/?d=M048P-N1.xml", "HEAD",  200, "text/html; charset=utf-8", 0l, 599, today, "Ok", 0, Category.Ok));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "http://www.deutschestextarchiv.de/rem/?d=M112-G1.xml", "HEAD",  200, "text/html; charset=utf-8", 0l, 591, today, "Ok", 0, Category.Ok));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "https://www.google.com", "HEAD",  200, "text/html; charset=ISO-8859-1", 0l, 222, today, "Ok", 0, Category.Ok));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "https://maps.google.com", "HEAD",  200, "text/html; charset=UTF-8", 0l, 440, today, "Ok", 2, Category.Ok));
+            Configuration.checkedLinkResource.save(new CheckedLink(null, null, "https://drive.google.com", "HEAD",  200, "text/html; charset=UTF-8", 73232l, 413, today, "Ok", 1, Category.Ok));
             
 
 
