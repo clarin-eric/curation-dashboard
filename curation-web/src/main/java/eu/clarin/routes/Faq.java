710f48a6b46a77eaaa4586ee346bf04811ec98fc
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2022-04-17 18:06:46 +0200

    externalising help and faq page

--- curation-web/src/main/java/eu/clarin/routes/Faq.java
+++ curation-web/src/main/java/eu/clarin/routes/Faq.java
@@ -1,6 +1,5 @@
 package eu.clarin.routes;
 
-import eu.clarin.helpers.FileManager;
 import eu.clarin.helpers.ResponseManager;
 import eu.clarin.main.Configuration;
 import lombok.extern.slf4j.Slf4j;
@@ -15,10 +14,7 @@ import org.commonmark.node.Node;
 import org.commonmark.parser.Parser;
 import org.commonmark.renderer.html.HtmlRenderer;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.Arrays;
@@ -31,9 +27,7 @@ public class Faq {
     @GET
     @Path("/")
     public Response getFaq() {
-        try {
-            String faq = FileManager.readFile(Configuration.VIEW_RESOURCES_PATH + "/html/faq.html");
-            
+        try {            
             List<Extension> extensions = Arrays.asList(TablesExtension.create());
             Parser parser = Parser.builder()
                     .extensions(extensions)
@@ -54,25 +48,10 @@ public class Faq {
                throw ex;
             }
  
-            return ResponseManager.returnHTML(200, faq.replace("<!-- replacement -->", renderer.render(document)));
-        } catch (IOException e) {
+            return ResponseManager.returnHTML(200, renderer.render(document));
+        } 
+        catch (IOException e) {
             log.error("Error when reading faq.html: ", e);
-            return ResponseManager.returnServerError();
-        }
-    }
-
-    @GET
-    @Path("/faq.md")
-    public Response getFaqMD() {
-
-        String faqJsonPath = Configuration.VIEW_RESOURCES_PATH + "/markdown/faq.md";
-        final InputStream fileInStream;
-        try {
-            fileInStream = new FileInputStream(faqJsonPath);
-            return ResponseManager.returnFile(200, fileInStream, "text/markdown", "faq.md");
-
-        } catch (FileNotFoundException e) {
-            log.error("There was an error getting the faq.md file: ", e);
             return ResponseManager.returnServerError();
         }
     }
--- curation-web/src/main/java/eu/clarin/routes/Help.java
+++ curation-web/src/main/java/eu/clarin/routes/Help.java
@@ -8,7 +8,18 @@ import lombok.extern.slf4j.Slf4j;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+
+import org.commonmark.Extension;
+import org.commonmark.ext.gfm.tables.TablesExtension;
+import org.commonmark.node.Node;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 
 @Slf4j
 @Path("/help")
@@ -17,13 +28,32 @@ public class Help {
     @GET
     @Path("/")
     public Response getHelp() {
-        try {
-            String help = FileManager.readFile(Configuration.VIEW_RESOURCES_PATH + "/html/help.html");
+       try {            
+          List<Extension> extensions = Arrays.asList(TablesExtension.create());
+          Parser parser = Parser.builder()
+                  .extensions(extensions)
+                  .build();
+          HtmlRenderer renderer = HtmlRenderer.builder()
+                  .extensions(extensions)
+                  .build();
+          
+          Node document = null;
+          URL markdown = new URL(Configuration.DOC_URL + "help.md");
+          try(InputStreamReader in = new InputStreamReader(markdown.openStream())){
+             
+             document = parser.parseReader(in);
+             
+          }
+          catch(Exception ex) {
+             log.error("error in call markdown page with URL {}", markdown);
+             throw ex;
+          }
 
-            return ResponseManager.returnHTML(200, help);
-        } catch (IOException e) {
-            log.error("Error when reading help.html: ", e);
-            return ResponseManager.returnServerError();
-        }
+          return ResponseManager.returnHTML(200, renderer.render(document));
+      } 
+      catch (IOException e) {
+          log.error("Error when reading help.html: ", e);
+          return ResponseManager.returnServerError();
+      }
     }
 }
