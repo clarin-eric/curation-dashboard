1c38770f2e6d30296ab1116676cf1910cb6981cd
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2022-11-10 14:33:21 +0100

    changing scope from prototype to default singleton

--- curation-api/src/main/java/eu/clarin/cmdi/curation/api/processor/CMDInstanceProcessor.java
+++ curation-api/src/main/java/eu/clarin/cmdi/curation/api/processor/CMDInstanceProcessor.java
@@ -10,12 +10,10 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.stream.Stream;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-@Scope("prototype")
 public class CMDInstanceProcessor {
 
    @Autowired
