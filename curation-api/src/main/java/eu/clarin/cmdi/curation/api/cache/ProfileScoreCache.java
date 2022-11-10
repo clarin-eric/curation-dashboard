79f77358dafa9f8a260b8653c5110a85f14d8c27
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2022-11-10 14:37:02 +0100

    instantiating CMDProfile.class with getBean instead of new

--- curation-api/src/main/java/eu/clarin/cmdi/curation/api/cache/ProfileScoreCache.java
+++ curation-api/src/main/java/eu/clarin/cmdi/curation/api/cache/ProfileScoreCache.java
@@ -1,6 +1,8 @@
 package eu.clarin.cmdi.curation.api.cache;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import eu.clarin.cmdi.curation.api.entity.CMDProfile;
@@ -12,10 +14,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ProfileScoreCache {
    
+   @Autowired
+   ApplicationContext ctx;
+   
    @Cacheable(value = "profileScores", condition = "#header.isPublic")
    public Double getScore(ProfileHeader header) {
       
-      CMDProfile profile = new CMDProfile(header.getSchemaLocation(), header.getCmdiVersion());       
+      CMDProfile profile = ctx.getBean(CMDProfile.class,header.getSchemaLocation(), header.getCmdiVersion());       
       log.trace("Calculating and caching score for {}", profile);
       
       try {
