dbb1183a0ff521fa78ad6c6a31e138ac41d24a06
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2022-11-13 19:30:34 +0100

    adding tests

--- curation-api/src/test/java/eu/clarin/cmdi/curation/api/InstanceTest.java
+++ curation-api/src/test/java/eu/clarin/cmdi/curation/api/InstanceTest.java
@@ -5,13 +5,18 @@
 package eu.clarin.cmdi.curation.api;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
@@ -24,22 +29,53 @@ import eu.clarin.cmdi.curation.api.report.Report;
  */
 @SpringBootTest
 @Import(TestConfig.class)
+@TestInstance(Lifecycle.PER_CLASS)
 public class InstanceTest {
+   
+   private Report<?> report;
    
    @Autowired
    CurationModule curation;
    
-   @Test
+   @BeforeAll
    public void createInstanceReport() throws URISyntaxException {
       
       URI instanceURI = this.getClass().getClassLoader().getResource("instance/SAW_Leipzig_Repository/cts_muqtabas_urn_cts_muqtabas_oclc_4770057679_i_29_TEIP5_.xml").toURI();
       Path instancePath = Paths.get(instanceURI);
       
-      Report<?> report = curation.processCMDInstance(instancePath);
+      this.report = curation.processCMDInstance(instancePath);
       
-      assertTrue(report instanceof CMDInstanceReport);
+   }
+   
+   @Test
+   @Order(1)
+   public void isInstanceReport() {
       
-      report.toXML(System.out);
+      assertTrue(this.report instanceof CMDInstanceReport);
+      
+   }
+   
+   @Test
+   @Order(2)
+   public void selectionReportsNotNull() {
+      
+      CMDInstanceReport instanceReport = CMDInstanceReport.class.cast(report);
+      
+      assertNotNull(instanceReport.header);
+      
+      assertNotNull(instanceReport.fileReport);
+      
+      assertNotNull(instanceReport.resProxyReport);
+      
+      assertNotNull(instanceReport.xmlPopulatedReport);
+      
+      assertNotNull(instanceReport.xmlValidityReport);
+      
+      assertNotNull(instanceReport.urlReport);
+      
+      assertNotNull(instanceReport.facets);
+      
+      this.report.toXML(System.out);
       
    }
 
--- curation-api/src/test/java/eu/clarin/cmdi/curation/api/ProfileTest.java
+++ curation-api/src/test/java/eu/clarin/cmdi/curation/api/ProfileTest.java
@@ -9,7 +9,11 @@ import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
@@ -19,25 +23,44 @@ import eu.clarin.cmdi.curation.api.report.Report;
 
 @SpringBootTest
 @Import(TestConfig.class)
-
+@TestInstance(Lifecycle.PER_CLASS)
 public class ProfileTest {
+   
+   private Report<?> report;
    
    @Autowired
    CurationModule curation;
    
-   @Test
-   public void createProfileReport() throws URISyntaxException, MalformedURLException {
+   @BeforeAll
+   public void createReport() throws URISyntaxException, MalformedURLException {
       
       URI profileURI = this.getClass().getClassLoader().getResource("profile/LexicalResourceProfile.xsd").toURI();
       Path profilePath = Paths.get(profileURI);
       
-      Report<?> report = curation.processCMDProfile(profilePath);
-      
+      this.report = curation.processCMDProfile(profilePath);
+   }
+   
+   @Test
+   @Order(1)
+   public void isProfileReport() throws URISyntaxException, MalformedURLException {
+       
       assertTrue(report instanceof CMDProfileReport);
       
+   }
+   
+   @Test
+   @Order(2)
+   public void sectionReportsNotNull() {
+        
       CMDProfileReport profileReport = CMDProfileReport.class.cast(report);
       
       assertNotNull(profileReport.header);
+      
+      assertNotNull(profileReport.components);
+      
+      assertNotNull(profileReport.elements);
+      
+      assertNotNull(profileReport.facet);
       
       
       //report.toXML(System.out);
