ff5c7e21000f965e591fe1bff8eb5fe79f732131
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2022-11-17 23:00:09 +0100

    adding annotations for ComponentScan etc.

--- curation-app/src/main/java/eu/clarin/cmdi/curation/app/CurationAppApplication.java
+++ curation-app/src/main/java/eu/clarin/cmdi/curation/app/CurationAppApplication.java
@@ -1,9 +1,21 @@
 package eu.clarin.cmdi.curation.app;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "eu.clarin.cmdi.cpa.repository")
+@EntityScan(basePackages = "eu.clarin.cmdi.cpa.model")
+@EnableCaching
+@EnableConfigurationProperties
+@ComponentScan("eu.clarin.cmdi")
+@EnableAutoConfiguration
 public class CurationAppApplication {
 
 	public static void main(String[] args) {
