0a925bbfc5b6811ba21d13eb170855f0ae47ac90
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2022-11-21 16:10:10 +0100

    replacing thrown Exception by specific SubprocessorException

--- curation-api/src/main/java/eu/clarin/cmdi/curation/api/subprocessor/AbstractSubprocessor.java
+++ curation-api/src/main/java/eu/clarin/cmdi/curation/api/subprocessor/AbstractSubprocessor.java
@@ -4,13 +4,14 @@
 package eu.clarin.cmdi.curation.api.subprocessor;
 
 import eu.clarin.cmdi.curation.api.entity.CMDInstance;
+import eu.clarin.cmdi.curation.api.exception.SubprocessorException;
 import eu.clarin.cmdi.curation.api.report.CMDInstanceReport;
 import eu.clarin.cmdi.curation.api.report.Score;
 
 
 public abstract class AbstractSubprocessor extends AbstractMessageCollection{
 
-    public abstract void process(CMDInstance entity, CMDInstanceReport report) throws Exception;
+    public abstract void process(CMDInstance entity, CMDInstanceReport report) throws SubprocessorException;
     
     public abstract Score calculateScore(CMDInstanceReport report);
 
