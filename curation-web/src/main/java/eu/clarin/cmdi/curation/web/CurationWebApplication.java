bcca2c83093918f3aab245677098a6c92ecde329
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2022-12-05 09:55:23 +0100

    adding FileTemplateResolver

--- curation-web/src/main/java/eu/clarin/cmdi/curation/web/CurationWebApplication.java
+++ curation-web/src/main/java/eu/clarin/cmdi/curation/web/CurationWebApplication.java
@@ -1,15 +1,33 @@
 package eu.clarin.cmdi.curation.web;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
 
 @SpringBootApplication
-@EnableAutoConfiguration
+@EnableAutoConfiguration@ComponentScan({"eu.clarin.cmdi.curation", "eu.clarin.linkchecker.persistence"})
+@EnableJpaRepositories(basePackages = "eu.clarin.linkchecker.persistence.repository")
+@EntityScan(basePackages = "eu.clarin.linkchecker.persistence.model")
 public class CurationWebApplication {
+   
+   @Autowired
+   TemplateEngine templateEngine;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurationWebApplication.class, args);
 	}
+	
+   @PostConstruct
+   public void init() {
+      templateEngine.addTemplateResolver(new FileTemplateResolver());
+   }
 
 }
