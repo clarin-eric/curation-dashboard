0e69625d7fde5ff598d99e01a2cfd90485853371
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2022-12-19 21:03:59 +0100

    allowing to run app in different modes

--- curation-app/src/main/java/eu/clarin/cmdi/curation/app/CurationApp.java
+++ curation-app/src/main/java/eu/clarin/cmdi/curation/app/CurationApp.java
@@ -4,20 +4,6 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.StandardCopyOption;
-import java.time.LocalDateTime;
-import java.util.stream.Stream;
-
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-import javax.xml.bind.util.JAXBSource;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -40,23 +26,20 @@ import eu.clarin.cmdi.curation.api.report.AllLinkcheckerReport;
 import eu.clarin.cmdi.curation.api.report.AllProfileReport;
 import eu.clarin.cmdi.curation.api.report.CMDProfileReport;
 import eu.clarin.cmdi.curation.api.report.CollectionReport;
-import eu.clarin.cmdi.curation.api.report.Report;
-import eu.clarin.cmdi.curation.api.utils.FileNameEncoder;
+import eu.clarin.cmdi.curation.api.utils.FileStorage;
 import eu.clarin.cmdi.curation.app.conf.AppConfig;
 import eu.clarin.cmdi.curation.pph.PPHService;
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@ComponentScan({"eu.clarin.cmdi.curation", "eu.clarin.linkchecker.persistence"})
 @EnableJpaRepositories(basePackages = "eu.clarin.linkchecker.persistence.repository")
 @EntityScan(basePackages = "eu.clarin.linkchecker.persistence.model")
 @EnableCaching
 @EnableConfigurationProperties
-@ComponentScan({"eu.clarin.cmdi.curation", "eu.clarin.linkchecker.persistence"})
 @EnableAutoConfiguration
 @Slf4j
 public class CurationApp {
-   
-   private final LocalDateTime generationDate = LocalDateTime.now();
    
    @Autowired
    private AppConfig conf;   
@@ -64,6 +47,8 @@ public class CurationApp {
    private CurationModule curation;
    @Autowired
    private PPHService pphService;
+   @Autowired
+   FileStorage storage;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurationApp.class, args);
@@ -71,131 +56,77 @@ public class CurationApp {
 	
    @Bean
    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-      return args -> {
+      return args -> {         
          
-         prepareOutPaths();
-         
-         
+         if("all".equalsIgnoreCase(conf.getMode()) || "profile".equalsIgnoreCase(conf.getMode())) {
                  
-         final AllProfileReport allProfileReport = new AllProfileReport();
-
-
-         pphService.getProfileHeaders().forEach(header -> {
-            
-
-            try {
-               CMDProfileReport profileReport = curation.processCMDProfile(header.getId());
+            final AllProfileReport allProfileReport = new AllProfileReport();
+   
+   
+            pphService.getProfileHeaders().forEach(header -> {
                
-               allProfileReport.addReport(profileReport);
+   
                try {
-                  dumpAsXML(profileReport, CurationEntityType.PROFILE);
-                  dumpAsHTML(profileReport, CurationEntityType.PROFILE);
+                  CMDProfileReport profileReport = curation.processCMDProfile(header.getId());
+                  
+                  allProfileReport.addReport(profileReport);
+                     
+                  storage.saveReport(profileReport, CurationEntityType.PROFILE, false);
+         
                }
-               catch (Exception e) {
+               catch (MalformedURLException e1) {
+                  
+                  log.error("malformed URL for id '{}' - check the setting for curation.pph-service.restApi");
+               }
+               catch (SubprocessorException e1) {
+   
+                  log.error("can't process public profile with profile id '{}'", header.getId());
+               }
+            });
+            
+            storage.saveReport(allProfileReport, CurationEntityType.PROFILE, false);
 
-                  log.error("can't dump report '{}'", profileReport.getName());                
-               
-               }           
-            }
-            catch (MalformedURLException e1) {
-               
-               log.error("malformed URL for id '{}' - check the setting for curation.pph-service.restApi");
-            }
-            catch (SubprocessorException e1) {
-
-               log.error("can't process public profile with profile id '{}'", header.getId());
-            }
-         });
-         
-         try {
-            dumpAsXML(allProfileReport, CurationEntityType.PROFILE);
-            dumpAsHTML(allProfileReport, CurationEntityType.PROFILE);
          }
-         catch (Exception e) {
-
-            log.error("can't dump report '{}'", e, allProfileReport.getName());                
          
-         }
+         if("all".equalsIgnoreCase(conf.getMode()) || "collection".equalsIgnoreCase(conf.getMode())) {
          
         
-         final AllCollectionReport allCollectionReport = new AllCollectionReport();
-         final AllLinkcheckerReport allLinkcheckerReport = new AllLinkcheckerReport();         
+            final AllCollectionReport allCollectionReport = new AllCollectionReport();
+            final AllLinkcheckerReport allLinkcheckerReport = new AllLinkcheckerReport();         
+   
+            conf.getDirectory().getIn().forEach(inPath -> processCollection(inPath, allCollectionReport,allLinkcheckerReport ));   
 
-         conf.getDirectory().getIn().forEach(inPath -> processCollection(inPath, allCollectionReport,allLinkcheckerReport ));
+            storage.saveReport(allCollectionReport, CurationEntityType.COLLECTION, true);
+            storage.saveReport(allLinkcheckerReport, CurationEntityType.LINKCHECKER, true);
 
-         try {
-            dumpAsXML(allCollectionReport, CurationEntityType.COLLECTION);
-            dumpAsHTML(allCollectionReport, CurationEntityType.COLLECTION);
-         }
-         catch (Exception e) {
-
-            log.error("can't dump report '{}'", e, allCollectionReport.getName());                
-         
          }
          
-         try {
-            dumpAsXML(allLinkcheckerReport, CurationEntityType.LINKCHECKER);
-            dumpAsHTML(allLinkcheckerReport, CurationEntityType.LINKCHECKER);
-         }
-         catch (Exception e) {
-
-            log.error("can't dump report '{}'", e, allLinkcheckerReport.getName());                
-         
+         if("all".equalsIgnoreCase(conf.getMode()) || "linkchecker".equalsIgnoreCase(conf.getMode())) {
+            
+            curation.getLinkcheckerDetailReports().forEach(report -> storage.saveReport(report, CurationEntityType.LINKCHECKER, false));
+ 
          }
       };
    }
    
    private void processCollection(Path path, AllCollectionReport allCollectionReport, AllLinkcheckerReport allLinkcheckerReport) {
       
-      try {
-         if(isCollectionRoot(path)) {
-            
-            CollectionReport colectionReport = curation.processCollection(path);
-            
-            allCollectionReport.addReport(colectionReport);
-            allLinkcheckerReport.addReport(colectionReport);
-            
-            dumpAsXML(colectionReport, CurationEntityType.COLLECTION);
-            dumpAsHTML(colectionReport, CurationEntityType.COLLECTION);
-            
-         }
-         else {
-            processCollection(path, allCollectionReport, allLinkcheckerReport);
-         }
-      }
 
-      catch (TransformerException e) {
+      if(isCollectionRoot(path)) {
          
-         log.error("", e);
+         CollectionReport colectionReport = curation.processCollection(path);
+         
+         allCollectionReport.addReport(colectionReport);
+         allLinkcheckerReport.addReport(colectionReport);
+         
+         storage.saveReport(colectionReport, CurationEntityType.COLLECTION, true);
+         
       }
-      catch (JAXBException e) {
-
-         log.error("", e);
-      }
- 
-   }
-   
-   private void prepareOutPaths() {
+      else {
+         
+         processCollection(path, allCollectionReport, allLinkcheckerReport);
       
-      Stream.of("xml", "html").forEach(dir -> {
-         
-         Stream.of(CurationEntityType.values()).forEach(subdir -> {
-            
-            Path outDir = conf.getDirectory().getOut().resolve(dir).resolve(subdir.getStringValue());
-            
-            if(Files.notExists(outDir)) {
-               
-               try {
-                  Files.createDirectories(outDir);
-               }
-               catch (IOException e) {
-                  
-                  log.error("can't create output directory '{}'", outDir);
-               
-               }
-            }     
-         });         
-      });      
+      }
    }
    
    private boolean isCollectionRoot(Path path) {
@@ -206,69 +137,6 @@ public class CurationApp {
       catch (IOException e) {
          
          log.error("can't walk through path '{}'", path);
-         throw new RuntimeException(e);
-      }
-   }
-   
-   private void dumpAsXML(Report<?> report, CurationEntityType type) throws JAXBException {
-      
-      JAXBContext jc = JAXBContext.newInstance(report.getClass());
-
-      Marshaller marshaller = jc.createMarshaller();
-      marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-      marshaller.setProperty(javax.xml.bind.Marshaller.JAXB_ENCODING, "UTF-8");
-
-      Path path = conf.getDirectory().getOut().resolve("xml").resolve(type.toString());
-
-      String filename = FileNameEncoder.encode(report.getName()) + ".xml";
-      path = path.resolve(filename);
-
-      try {
-         marshaller.marshal(report, Files.newOutputStream(path));
-      }
-      catch (IOException e) {
-         
-         log.error("can't create/write to file '{}'", path);
-         throw new RuntimeException(e);
-      
-      }
-
-      // instead of a rollover we create a copy with report generation date in
-      // filename
-      copyStampedFile(path);
-   
-   }
-
-   private void dumpAsHTML(Report<?> report, CurationEntityType type) throws TransformerException, JAXBException {
-      
-      Path path = this.conf.getDirectory().getOut().resolve("html").resolve(type.toString());
-
-      String filename = FileNameEncoder.encode(report.getName()) + ".html";
-      path = path.resolve(filename);
-
-      TransformerFactory factory = TransformerFactory.newInstance();
-
-      Source xslt = new StreamSource(
-            this.getClass().getResourceAsStream("/xslt/" + report.getClass().getSimpleName() + "2HTML.xsl"));
-
-      Transformer transformer = factory.newTransformer(xslt);
-      transformer.transform(new JAXBSource(JAXBContext.newInstance(report.getClass()), report),
-            new StreamResult(path.toFile()));
-
-      copyStampedFile(path);
-
-   }
-
-   private void copyStampedFile(Path path) {
-      String stampedFileName = path.getFileName().toString().replace(".",
-            "_" + String.format("%1tF", generationDate) + ".");
-      Path stampedPath = path.getParent().resolve(stampedFileName);
-      try {
-         Files.copy(path, stampedPath, StandardCopyOption.REPLACE_EXISTING);
-      }
-      catch (IOException e) {
-         
-         log.error("can't copy file '{}' to stmped file '{}'", path, stampedPath);
          throw new RuntimeException(e);
       }
    }
