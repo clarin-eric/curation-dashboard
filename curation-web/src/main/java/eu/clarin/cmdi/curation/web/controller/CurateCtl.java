fbbe8fd2a42607f407b711de1fb7c5c52e5a56a4
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2023-03-02 20:56:58 +0100

    corrections

--- curation-web/src/main/java/eu/clarin/cmdi/curation/web/controller/CurateCtl.java
+++ curation-web/src/main/java/eu/clarin/cmdi/curation/web/controller/CurateCtl.java
@@ -11,7 +11,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -21,10 +20,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -43,7 +41,9 @@ public class CurateCtl {
    FileStorage storage;
 
    @GetMapping()
-   public String getInstanceQueryParam(@RequestParam(name="url-input", required=false) String urlStr, Model model) {      
+   public String getInstanceQueryParam(@RequestParam(name="url-input", required=false) String urlStr, Model model) {  
+      
+      log.debug("urlStr: {}", urlStr);
 
       if (urlStr == null || urlStr.isEmpty()) {
          model.addAttribute("insert", "fragments/validator.html");
@@ -55,11 +55,12 @@ public class CurateCtl {
          // just to make it more user friendly ignore leading and trailing spaces
          urlStr = urlStr.trim();
 
-
-         if (!urlStr.startsWith("http://") && !urlStr.startsWith("https://")) {
-            if (urlStr.startsWith("www")) {
-               urlStr = "http://" + urlStr;
-            }   
+         if (urlStr.startsWith("www")) {
+            urlStr = "http://" + urlStr;
+         }         
+         
+         if (urlStr.matches("http(s)?://.+")) {
+  
 
             try {
                inFilePath = Files.createTempFile(FileNameEncoder.encode(urlStr.toString()), "xml");
@@ -79,19 +80,19 @@ public class CurateCtl {
             }
          }
          else {
-            
-            inFilePath = conf.getDirectory().getDataRoot().resolve(urlStr);// here it is regarded as a path instead of url.
 
-            if (Files.notExists(inFilePath)) {
+            inFilePath = conf.getDirectory().getDataRoot().resolve(urlStr).normalize();// here it is regarded as a path instead of url.
+            log.debug("inFilePath: {}", inFilePath);
+            if (Files.notExists(inFilePath) || !inFilePath.startsWith(conf.getDirectory().getDataRoot())) {
                
-               log.error("no valid unput file with path '{}'", inFilePath);
+               log.error("no valid input file with path '{}'", inFilePath);
                throw new RuntimeException("Given URL is invalid");
             } // else go down and curate the file
          }
          
          Path htmlFilePath = curate(inFilePath);
          
-         model.addAttribute("include", htmlFilePath.toString());       
+         model.addAttribute("insert", htmlFilePath.toString());       
          
       }
       return "generic";
@@ -124,46 +125,49 @@ public class CurateCtl {
 
    private Path curate(Path inFilePath) {
 
-      NamedReport report;
+      NamedReport report = null;
       
       CurationEntityType entityType;
       
-      StringBuffer buffer = new StringBuffer();
       
-      try(BufferedReader reader = Files.newBufferedReader(inFilePath)){
-         String line = null; 
+      byte[] buffer = new byte[200];
+      
+      try(InputStream inStream = Files.newInputStream(inFilePath)){
          
-         while((line = reader.readLine()) != null && buffer.length() < 200) {
-            
-            buffer.append(line.subSequence(0, 200));
+         inStream.read(buffer);
+         
+         String text = new String(buffer);
+         
+         if (text.contains("xmlns:xs=")) {// it's a profile
+            if (!text.contains("http://www.clarin.eu/cmd/1")) { // but not a valid cmd 1.2 profile
+               throw new RuntimeException("profile has no cmd 1.2 namespace declaration");
+            }
+            else {
+               
+               entityType = CurationEntityType.PROFILE;
+               try {
+                  report = curation.processCMDProfile(inFilePath.toUri().toURL());
+               }
+               catch (MalformedURLException e) {
+                  
+                 new RuntimeException(e);
+               }   
+            }
          }
+         else { // no profile - so processed as CMD instance
+            entityType = CurationEntityType.INSTANCE;
+            report = curation.processCMDInstance(inFilePath);
+         }
+
+         storage.saveReportAsXML(report, entityType);
+
+         return storage.saveReportAsHTML(report, entityType);
+
       }
       catch (IOException e) {
 
          log.error("can't read file '{}'", inFilePath);
-         throw new RuntimeException("internal error - please inform Clarin-Eric");
-         
+         throw new RuntimeException("internal error - please inform Clarin-Eric");     
       }
-      
-
-      if (buffer.toString().contains("xmlns:xs=")) {// it's a profile
-         if (!buffer.toString().contains("http://www.clarin.eu/cmd/1")) { // but not a valid cmd 1.2 profile
-            throw new RuntimeException("profile has no cmd 1.2 namespace declaration");
-         }
-         else {
-            
-            entityType = CurationEntityType.PROFILE;
-            report = curation.processCMDInstance(inFilePath);
-         
-         }
-      }
-      else { // no profile - so processed as CMD instance
-         entityType = CurationEntityType.INSTANCE;
-         report = curation.processCMDInstance(inFilePath);
-      }
-
-      storage.saveReportAsXML(report, entityType);
-
-      return storage.saveReportAsHTML(report, entityType);
    }
 }
