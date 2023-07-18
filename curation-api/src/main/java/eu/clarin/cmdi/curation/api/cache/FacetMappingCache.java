9ab3912ee9e6ff27bc6961957018d2973672c14d
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2023-07-18 12:12:48 +0200

    removing redundant import

--- curation-api/src/main/java/eu/clarin/cmdi/curation/api/cache/FacetMappingCache.java
+++ curation-api/src/main/java/eu/clarin/cmdi/curation/api/cache/FacetMappingCache.java
@@ -1,20 +1,15 @@
 package eu.clarin.cmdi.curation.api.cache;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
 
 import eu.clarin.cmdi.curation.api.vlo_extension.FacetsMappingCacheFactory;
-import eu.clarin.cmdi.curation.cr.CRService;
 import eu.clarin.cmdi.curation.pph.ProfileHeader;
 import eu.clarin.cmdi.vlo.importer.mapping.FacetsMapping;
 
 
 @Component
 public class FacetMappingCache {
-   
-   @Autowired
-   CRService crService;
    
    @Cacheable(value = "publicFacetMappingCache", key = "#header.id")
    public FacetsMapping getPublicFacetMapping(ProfileHeader header, FacetsMappingCacheFactory fac) {
