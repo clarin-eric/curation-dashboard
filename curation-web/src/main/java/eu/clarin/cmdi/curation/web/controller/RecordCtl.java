71e16512c2dac3abe63a6c066ce21e4d94f74f6f
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2023-10-04 16:08:52 +0200

    issue #203

--- curation-web/src/main/java/eu/clarin/cmdi/curation/web/controller/RecordCtl.java
+++ curation-web/src/main/java/eu/clarin/cmdi/curation/web/controller/RecordCtl.java
@@ -12,11 +12,13 @@ import java.nio.file.Path;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 
 import eu.clarin.cmdi.curation.api.conf.ApiConfig;
@@ -46,7 +48,7 @@ public class RecordCtl {
       
       if(Files.notExists(cmdiPath) || !cmdiPath.startsWith(conf.getDirectory().getDataRoot())) {
          
-         throw new RuntimeException("no CMD instance '" + origin + "'");
+         throw new ResponseStatusException(HttpStatus.NOT_FOUND, "entity not found");         
       }
       
       StreamingResponseBody stream = outputStream -> {
@@ -63,8 +65,7 @@ public class RecordCtl {
          catch (IOException e) {
             
             log.error("can't read CMD instance '{}'", cmdiPath);
-            throw new RuntimeException("internal error - please contact Clarin");
-         
+            throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR);              
          }      
       };
       
