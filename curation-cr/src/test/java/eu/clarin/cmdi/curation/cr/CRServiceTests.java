d5f11d5c477830666ae4b5e043ad75454b4f936e
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2024-08-11 11:02:59 +0200

    issue #244

--- curation-cr/src/test/java/eu/clarin/cmdi/curation/cr/CRServiceTests.java
+++ curation-cr/src/test/java/eu/clarin/cmdi/curation/cr/CRServiceTests.java
@@ -2,7 +2,12 @@ package eu.clarin.cmdi.curation.cr;
 
 import eu.clarin.cmdi.curation.cr.conf.CRConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.mockserver.client.MockServerClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -13,59 +18,70 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
+import javax.net.ssl.HttpsURLConnection;
+
+import java.io.File;
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
+
 @SpringBootTest()
-//@Import(CRServiceTests.TestConfig.class)
+@Import(CRServiceTests.TestConfig.class)
 @EnableConfigurationProperties
 @EnableAutoConfiguration
 @ComponentScan(basePackages = "eu.clarin.cmdi.curation")
 @Slf4j
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CRServiceTests {
    
    @Autowired
    CRService crService;   
    @Autowired
-   CRConfig crProps;
+   CRConfig crConfig;
    @Autowired
    CacheManager cacheManager;
-/*
-	@Test
-	void getCRService() throws PPHServiceNotAvailableException {
 
-	   
-	   phService.getProfileHeaders().stream().limit(5).forEach(header -> {
+    @BeforeAll
+    public void prepareFileCache() throws IOException {
+
+        FileUtils.copyDirectory(new File(this.getClass().getResource("/cr_cache").getFile()), crConfig.getCrCache().toFile());
+    }
+
+	@Test
+    @Order(1)
+	void isPublic()  {
 
 	      try {
             
-            assertEquals(crService.getParsedProfile(header).getId(), header.getId()); 
-         
+            assertTrue(crService.getParsedProfile("https://catalog.clarin.eu/ds/ComponentRegistry/rest/registry/1.x/profiles/clarin.eu:cr1:p_1403526079381/xsd").header().isPublic() );
+//            assertFalse(crService.getParsedProfile("https://catalog.clarin.eu/ds/ComponentRegistry/rest/registry/1.x/profiles/clarin.eu:cr1:p_1403526079382/xsd").header().isPublic() );
 	      }
          catch (Exception e) {
-            log.error("error in schema '{}'", header.getId());
+
+            log.error("error in schema");
             log.error("", e);
          }
-	   });   
 	}
 	
 	@Test
-	void useUache() throws PPHServiceNotAvailableException {
-	   
-      phService.getProfileHeaders().stream().limit(5).forEach(header -> {
+    @Order(2)
+	void cacheUsage() {
 
          try {
-            crService.getParsedProfile(header);
             
-            assertNotNull(cacheManager.getCache("publicProfileCache").get(header.getId()));
-             
+//            assertNotNull(cacheManager.getCache("profileCache").get("https://catalog.clarin.eu/ds/ComponentRegistry/rest/registry/1.x/profiles/clarin.eu:cr1:p_1403526079381/xsd"));
+//            assertNotNull(cacheManager.getCache("profileCache").get("https://catalog.clarin.eu/ds/ComponentRegistry/rest/registry/1.x/profiles/clarin.eu:cr1:p_1403526079382/xsd"));
+
          }
          catch (Exception e) {
-            log.error("error in schema '{}'", header.getId());
+
+            log.error("error in schema");
             log.error("", e);
          }
-      });  	   
-	   
 	}
 	
 	@SpringBootConfiguration
@@ -73,6 +89,4 @@ class CRServiceTests {
 	public static class TestConfig {
 
 	}
-
- */
 }
