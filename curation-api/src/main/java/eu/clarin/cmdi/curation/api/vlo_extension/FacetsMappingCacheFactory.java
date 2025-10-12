32132d2f4c4628346b26aa24b1dbeb83c7e2862c
WolfgangWalter Sauer (wowasa) <clarin@wowasa.com>
2025-10-12 16:28:19 +0200

    switching logging for FacetMappingFactory off (issue #290)

--- curation-api/src/main/java/eu/clarin/cmdi/curation/api/vlo_extension/FacetsMappingCacheFactory.java
+++ curation-api/src/main/java/eu/clarin/cmdi/curation/api/vlo_extension/FacetsMappingCacheFactory.java
@@ -15,10 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * The type Facets mapping cache factory.
